0552564a09fa0d2e379b13e7ae258ce60c0e82a2
xavimarin35 <36265010+xavimarin35@users.noreply.github.com>
2019-11-11 14:41:44 +0100

    different explosions when particle collides with an enemy

--- Thalassa/Motor2D/j1Particle.cpp
+++ Thalassa/Motor2D/j1Particle.cpp
@@ -19,18 +19,34 @@ j1Particle::j1Particle()
 	for (uint i = 0; i < MAX_ACTIVE_PARTICLES; ++i)
 		active[i] = nullptr;
 
+	//basic
 	basicShoot.anim.PushBack({ 65,75,8,6 });
 	basicShoot.anim.PushBack({ 77,74,8,7 });
 	basicShoot.anim.speed = 0.1f;
 	basicShoot.life = 2000;
 	basicShoot.type = BASIC_SHOOT;
 
+	basicShootDestroyed.anim.PushBack({ 88,73,9,9 });
+	basicShootDestroyed.anim.PushBack({ 100,73,12,9 });
+	basicShootDestroyed.anim.PushBack({ 113,73,12,11 });
+	basicShootDestroyed.anim.PushBack({ 129,71,14,12 });
+	basicShootDestroyed.anim.speed = 0.1f;
+	basicShootDestroyed.life = 200;
+
+	//remote
 	remoteShoot.anim.PushBack({ 52,25,11,6 });
 	remoteShoot.anim.PushBack({ 70,24,9,7 });
 	remoteShoot.anim.speed = 0.1f;
 	remoteShoot.life = 2000;
 	remoteShoot.type = REMOTE_SHOOT;
 
+	remoteShootDestroyed.anim.PushBack({ 83,19,11,17 });
+	remoteShootDestroyed.anim.PushBack({ 103,17,14,21 });
+	remoteShootDestroyed.anim.PushBack({ 125,17,14,19 });
+	remoteShootDestroyed.anim.speed = 0.1f;
+	remoteShootDestroyed.life = 200;
+
+	//demon
 	demonShoot.anim.PushBack({ 117,184,9,3 });
 	demonShoot.anim.PushBack({ 153,184,5,3 });
 	demonShoot.anim.speed = 0.1f;
@@ -40,7 +56,8 @@ j1Particle::j1Particle()
 	demonShootDestroyed.anim.PushBack({ 186,181,4,8 });
 	demonShootDestroyed.anim.PushBack({ 153,184,5,3 });
 	demonShootDestroyed.anim.speed = 0.1f;
-	demonShootDestroyed.anim.loop = false;
+	demonShootDestroyed.life = 200;
+
 
 }
 
@@ -51,13 +68,16 @@ j1Particle::~j1Particle()
 bool j1Particle::Start()
 {
 	LOG("Loading particles");
-	/*part_tex = App->tex->Load("textures/Particles/particles.png");*/
 	part2_tex = App->tex->Load("textures/Particles/shots.png");
 	demonShot_tex = App->tex->Load("textures/Enemies/Demon_Spritesheet.png");
 
 	basicShoot.tex = part2_tex;
+	basicShootDestroyed.tex = part2_tex;
 	remoteShoot.tex = part2_tex;
+	remoteShootDestroyed.tex = part2_tex;
 	demonShoot.tex = demonShot_tex;
+	demonShootDestroyed.tex = demonShot_tex;
+
 
 	return true;
 }
@@ -72,8 +92,12 @@ bool j1Particle::CleanUp()
 	{
 		if (active[i] != nullptr)
 		{
-			delete active[i];
-			active[i] = nullptr;
+			if (active[i]->type == BASIC_SHOOT)
+			{
+				App->tex->UnLoad(part2_tex);
+				delete active[i];
+				active[i] = nullptr;
+			}
 		}
 	}
 	return true;
@@ -146,35 +170,38 @@ void j1Particle::AddParticle(const Particle& particle, int x, int y, float dt, C
 void j1Particle::OnCollision(Collider* c1, Collider* c2)
 {
 	bool particleDestroyed = false;
-	
-		int ret = true;
-		for (uint i = 0; i < MAX_ACTIVE_PARTICLES && ret; ++i)
+		if (c2->type == COLLIDER_ENEMY) 
 		{
-			if (active[i] != nullptr) {
-				if (active[i]->collider == c1) {
-					switch (active[i]->type) {
-					case BASIC_SHOOT:
-						break;
-					case REMOTE_SHOOT:
-						break;
+			int ret = true;
+			for (uint i = 0; i < MAX_ACTIVE_PARTICLES && ret; ++i)
+			{
+				if (active[i] != nullptr) {
+					if (active[i]->collider == c1) {
+						switch (active[i]->type) {
+						case BASIC_SHOOT:
+								AddParticle(demonShootDestroyed, active[i]->position.x, active[i]->position.y, 0);
+								particleDestroyed = true;
+							break;
+						case REMOTE_SHOOT:
+								AddParticle(demonShootDestroyed, active[i]->position.x, active[i]->position.y, 0, COLLIDER_SHOOT, 0, (double)-90.0f, REMOTE_SHOOT);
+								particleDestroyed = true;
+							break;
+						}
+						ret = false;
 					}
-					ret = false;
 				}
 			}
 		}
+		
 	
-
+// destroy the particle collided
 	for (uint i = 0; i < MAX_ACTIVE_PARTICLES; ++i)
 	{
-		// Always destroy particles that collide
 		if (active[i] != nullptr && active[i]->collider == c1 && particleDestroyed)
 		{
-			/*App->particles->AddParticle(App->particles->demonShootDestroyed, )*/
-			//AddParticle(...) ---> If we want to print an explosion when hitting an enemy
 			delete active[i];
 			active[i] = nullptr;
 			break;
-
 		}
 
 	}
