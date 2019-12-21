733dcc34474382710e2a5b5c3c1b408eef614323
Bullseye14 <36154277+Bullseye14@users.noreply.github.com>
2019-12-21 12:29:46 +0100

    new pngs for mainmenu background

--- Thalassa/Motor2D/j1MainMenu.cpp
+++ Thalassa/Motor2D/j1MainMenu.cpp
@@ -41,15 +41,13 @@ bool j1MainMenu::Awake(pugi::xml_node &)
 
 bool j1MainMenu::Start()
 {
-	texture = App->tex->Load("gui/BG_MAINMENU.png");
+	App->map->Load("MainMenu.tmx");
 
 	return true;
 }
 
 bool j1MainMenu::Update(float dt)
 {
-	SDL_Rect bg_rect = { 0,0,342,256 };
-	App->render->Blit(texture, 0, 0, &bg_rect);
 
 	if (App->input->GetKey(SDL_SCANCODE_F7) == KEY_DOWN)
 	{
